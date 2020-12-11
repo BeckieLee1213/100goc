60bbff8c133c71c1e8d0ee26f1163e89899c7aac
Beckie Fischer <25963373+beckiefischer73@users.noreply.github.com>
2020-12-11 08:48:19 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,91 @@
+info.onCountdownEnd(function () {
+    game.over(true)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    fish.setPosition(randint(0, scene.screenWidth()), randint(0, scene.screenHeight()))
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    pause(2000)
+})
+let fish: Sprite = null
+scene.setBackgroundColor(9)
+let duck = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . b 5 5 b . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . b b b b b 5 5 5 5 5 5 5 b . . 
+    . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+    . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+    . . b d 5 5 b 1 f f 5 4 4 c . . 
+    b b d b 5 5 5 d f b 4 4 4 4 b . 
+    b d d c d 5 5 b 5 4 4 4 4 4 4 b 
+    c d d d c c b 5 5 5 5 5 5 5 b . 
+    c b d d d d d 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+duck.setPosition(80, 60)
+fish = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . c c c c c c . . . 
+    . . . . . . c 5 5 5 5 5 c c . . 
+    . . . . . c 5 5 5 5 5 5 5 5 c . 
+    . . . . c b b b b b b 5 5 5 c . 
+    . . . . c b b b b 1 b b c c . . 
+    . . . . c 1 1 b b 1 1 1 c . . . 
+    . . . c 1 1 1 1 b 1 1 1 c . . . 
+    . . . c 1 1 1 1 b 1 1 1 b b c c 
+    . . c c d 1 1 1 b 1 b 1 5 5 5 c 
+    . . c c d 1 c 1 1 1 b 1 b b 5 c 
+    . c c d d 1 1 1 1 1 b 1 f b 5 c 
+    f d d d 1 1 1 1 1 b b b f . c c 
+    f f f f f 1 1 1 b b 5 5 5 f . . 
+    . . . . . f f f 5 5 5 5 5 f . . 
+    . . . . . . . . f f f f f f . . 
+    `, SpriteKind.Food)
+fish.setPosition(randint(0, scene.screenWidth()), randint(0, scene.screenHeight()))
+let shark = sprites.create(img`
+    .............ccfff..............
+    ...........ccddbcf..............
+    ..........ccddbbf...............
+    ..........fccbbcf...............
+    .....fffffccccccff.........ccc..
+    ...ffbbbbbbbcbbbbcfff....ccbbc..
+    ..fbbbbbbbbcbcbbbbcccff.cdbbc...
+    ffbbbbbbffbbcbcbbbcccccfcdbbf...
+    fbcbbb11ff1bcbbbbbcccccffbbf....
+    fbbb11111111bbbbbcccccccbbcf....
+    .fb11133cc11bbbbcccccccccccf....
+    ..fccc31c111bbbcccccbdbffbbcf...
+    ...fc13c111cbbbfcddddcc..fbbf...
+    ....fccc111fbdbbccdcc.....fbbf..
+    ........ccccfcdbbcc........fff..
+    .............fffff..............
+    `, SpriteKind.Enemy)
+shark.setPosition(200, randint(0, scene.screenHeight()))
+info.setScore(0)
+info.startCountdown(30)
+info.setLife(3)
+game.onUpdate(function () {
+    if (controller.left.isPressed()) {
+        duck.x += -5
+    } else if (controller.right.isPressed()) {
+        duck.x += 5
+    } else if (controller.up.isPressed()) {
+        duck.y += -5
+    } else if (controller.down.isPressed()) {
+        duck.y += 5
+    }
+})
+forever(function () {
+    while (shark.x >= 0) {
+        shark.x += -5
+        pause(100)
+    }
+    shark.setPosition(200, randint(0, scene.screenHeight()))
+})
